1877ec9ffb638cfa168e103561c7b85e1ea07d74
vgaware <gawarevaibhav125@gmail.com>
2024-11-22 13:18:52 +0530

    Test

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -5,7 +5,7 @@ import conf from '../conf/conf'
 
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
-    return (
+    return (        
         <div className='w-full'>
             {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
             <Controller
